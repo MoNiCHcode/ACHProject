a445d398dd6acf579c5e6b3315420a4232cf8a9b
爱享到 <aixiangdao@aixiangdaodeiMac.local>
2017-07-05 09:34:50 +0800

    Initial Commit

---
+++ ACHProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ACHProject
+//
+//  Created by 爱享到 on 2017/7/5.
+//  Copyright © 2017年 爱享到. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
